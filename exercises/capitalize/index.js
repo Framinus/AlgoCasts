5e1a41e0b6cf10995c321a0e004fbd912921ba96
James McCormack <thejamesmccormack@gmail.com>
2018-01-31 18:56:42 -0800

    add second solution to capitalize algorithm

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,29 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+// this time, we are going to make a string (no array needed). we will check each letter of the original string to see if it has a space to the left of it. if it does, we capitalize it.
+// function capitalize(str) {
+//   let newString = str[0].toUpperCase();
+//   for (let i = 1; i < str.length; i += 1) {
+//     if (str[i - 1] === ' ') {
+//       newString += str[i].toUpperCase()
+//     } else {
+//       newString += str[i];
+//     }
+//   }
+//   return newString;
+// }
+
+// this is the strongly preferred solution!
+function capitalize(str) {
+  // create a words array.
+  const words = [];
+  // split the string at white spaces and loop through each word, making the first character uppercase and slicing off the rest of the word (so getting rid of the original first character) and then joining the uppercase first letter to the rest of the word.
+  for (word of str.split(' ')) {
+    words.push(word[0].toUpperCase() + word.slice(1))
+  }
+  // then join the words array with white spaces. tada!
+  return words.join(' ')
+}
 
 module.exports = capitalize;
